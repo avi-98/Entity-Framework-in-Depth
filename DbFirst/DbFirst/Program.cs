eb7fc00e7823167e51e7399ac721aa7290b84166
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-01 13:12:08 -0500

    Section 3 Lecture 17: Import Stored Procedures

--- DbFirst/DbFirst/Program.cs
+++ DbFirst/DbFirst/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace DbFirst
+﻿using System;
+
+namespace DbFirst
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var dbContext = new PlutoDbContext();
+
+            var courses = dbContext.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
         }
     }
 }
