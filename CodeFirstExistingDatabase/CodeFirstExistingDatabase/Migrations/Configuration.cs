bde83a189991c280fbbea70128eb47504ac78499
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-05 09:36:51 -0500

    Modified Seed Method
    Modified the Seed method for Lecture 31

--- CodeFirstExistingDatabase/CodeFirstExistingDatabase/Migrations/Configuration.cs
+++ CodeFirstExistingDatabase/CodeFirstExistingDatabase/Migrations/Configuration.cs
@@ -1,4 +1,6 @@
-﻿namespace CodeFirstExistingDatabase.Migrations
+﻿using System.Collections.Generic;
+
+namespace CodeFirstExistingDatabase.Migrations
 {
     using System;
     using System.Data.Entity;
@@ -14,10 +16,15 @@
 
         protected override void Seed(CodeFirstExistingDatabase.PlutoContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author()
+                {
+                    Name = "Author 1",
+                    Courses = new List<Course>()
+                    {
+                        new Course(){Name = "Course for Author 1", Description = "Description"}
+                    }
+                });
         }
     }
 }
