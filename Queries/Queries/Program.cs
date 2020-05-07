5c97bd057cf4af3e74d4783359809d47c2db5d51
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-07 13:37:23 -0500

    Section 6 Lecture 48 Inner Join

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -9,15 +9,10 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            var query = from c in context.Courses
-                group c by c.Level
-                into g
-                select g;
-
-            foreach (var group in query)
-            {
-                Console.WriteLine($@"{group.Key} ({group.Count()})");
-            }
+            var query = 
+                from c in context.Courses
+                join a in context.Authors on c.AuthorId equals a.Id
+                select new {CourseName = c.Name, AuthorName = a.Name};
         }
     }
 }
