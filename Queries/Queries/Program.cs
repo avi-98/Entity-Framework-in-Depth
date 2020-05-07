0b801db9b861afde6b706b01db4084b9fb29f1bf
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-07 14:40:49 -0500

    Section 6 Lecture 50 Aggregating

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -9,8 +9,12 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            var allAbove10Dollars = context.Courses.All(c => c.FullPrice > 10);
-            context.Courses.Any(c => c.Level == 1);
+            var count = context.Courses.Where(c => c.Level == 1).Count();
+            var count2 = context.Courses.Count(c => c.Level == 1); // Filtering the count for courses that are level one
+
+            context.Courses.Max(c => c.FullPrice);
+            context.Courses.Min(c => c.FullPrice);
+            context.Courses.Average(c => c.FullPrice);
         }
     }
 }
