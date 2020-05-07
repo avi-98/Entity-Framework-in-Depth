b53b32b0aef7bdaf70020cdbaa31cdd215f9069e
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-07 13:22:38 -0500

    Section 6 Lecture 47 Demo Complete

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -1,10 +1,33 @@
-﻿
+﻿using System;
+using System.Diagnostics.Eventing;
+using System.Linq;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            // LINQ Syntax
+            var query = from c in context.Courses
+                where c.Name.Contains("C#")
+                orderby c.Name
+                select c;
+
+            foreach (var course in query)
+                Console.WriteLine(course.Name);
+
+            // Extension Methods
+            var courses = context.Courses
+                .Where(c => c.Name.Contains("C#"))
+                .OrderBy(c => c.Name);
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Name);
+            }
         }
     }
 }
