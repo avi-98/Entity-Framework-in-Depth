394e1e6c01502993610085922200208e425eeae9
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-08 11:21:29 -0500

    Exercise 6 Action Movies Sorted by Name

--- Section 6 Exercise Files/Vidzy/Vidzy/Program.cs
+++ Section 6 Exercise Files/Vidzy/Vidzy/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Vidzy
+﻿using System;
+using System.Linq;
+
+namespace Vidzy
 {
     class Program
     {
         static void Main(string[] args)
         {
             var context = new VidzyContext();
+
+            // Action movies sorted by name
+            var moviesSortedByName = context.Videos
+                .Where(v => v.GenreId == 2)
+                .OrderBy(v => v.Name);
+            foreach (var video in moviesSortedByName)
+                Console.WriteLine(video.Name);
         }
     }
 }
