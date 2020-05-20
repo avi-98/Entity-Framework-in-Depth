325335072969959a657ee07726d8683a1e2c6b2a
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-20 11:36:49 -0500

    Exercise 7 Updated to Latest Nuget Packages and .NET Framework 4.8

---
+++ Section 7 Exercise Files/Vidzy/Vidzy/Migrations/202005201635182_FirstRun.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidzy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstRun : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Tags", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Tags", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+    }
+}
