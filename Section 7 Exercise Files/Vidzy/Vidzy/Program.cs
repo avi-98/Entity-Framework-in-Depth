5d7fa0d7eba23b7b5ec8b02a1f4bcd365ffb7dfe
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-20 11:40:14 -0500

    Exercise 7 Step 1 Complete

--- Section 7 Exercise Files/Vidzy/Vidzy/Program.cs
+++ Section 7 Exercise Files/Vidzy/Vidzy/Program.cs
@@ -1,11 +1,22 @@
 ﻿
 
+using System;
+using System.Linq;
+
 namespace Vidzy
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new VidzyContext();
+
+            var videos = context.Videos.ToList();
+
+            foreach (var video in videos)
+            {
+                Console.WriteLine($"Video Name: {video.Name}\t Video Genre: {video.Genre.Name}");
+            }
         }
     }
 }
