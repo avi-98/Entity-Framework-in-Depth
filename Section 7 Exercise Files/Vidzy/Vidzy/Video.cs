46ed52d0784dda724c56ddf694ce550fe909018f
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-20 11:54:00 -0500

    Exercise 7 Step 2 Complete

--- Section 7 Exercise Files/Vidzy/Vidzy/Video.cs
+++ Section 7 Exercise Files/Vidzy/Vidzy/Video.cs
@@ -8,7 +8,7 @@ namespace Vidzy
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public Genre Genre { get; set; }
+        public virtual Genre Genre { get; set; }
         public byte GenreId { get; set; }
         public Classification Classification { get; set; }
         public ICollection<Tag> Tags { get; private set; }
