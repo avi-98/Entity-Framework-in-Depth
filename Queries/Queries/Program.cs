54d3ef728e780690dc5e14856cf493a081243c6a
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-20 14:57:29 -0500

    Section 8 Lecture 65 Updating Objects

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -10,19 +10,12 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            var authors = context.Authors.ToList();
-            var author = context.Authors.Single(a => a.Id == 1);
-
-            var course = new Course
+            var course = context.Courses.Find(4); // .Single(c => c.Id == 4)
+            if (course != null)
             {
-                Name = "New Course 2",
-                Description = "New Description",
-                FullPrice = 19.95f,
-                Level = 1,
-                Author = author
-            };
-
-            context.Courses.Add(course);
+                course.Name = "New Name";
+                course.AuthorId = 2;
+            }
 
             context.SaveChanges();
         }
